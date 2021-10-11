210cbcd52f801941197d2cb9997c3bcea8c5e2cb
Eddi3Ho <eddieho2001@gmail.com>
2021-10-11 14:51:35 +0800

    Initial commit

---
+++ application/controllers/Template.php
@@ -0,0 +1,65 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Template extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        // $this->load->model([
+        //     'user_student_model', 'user_ep_model', 'user_ac_model', 'user_ea_model', 'user_e_model',
+        //     'course_applicants_model', 'user_model', 'universities_model', 'employer_projects_model',
+        //     'rd_projects_model'
+        // ]);
+
+        // // Checks if session is set and if user is signed in as Admin (authorised access). If not, deny his/her access.
+        // if (!$this->session->userdata('user_id') || $this->session->userdata('user_role') != "Admin") {
+        //     redirect('user/login/Auth/login');
+        // }
+
+        // // Checks if session is set and if user signed in is not admin. Direct them back to their own dashboard.
+        // if ($this->session->has_userdata('has_login') && $this->session->userdata('user_role') != "Admin"  ){  
+
+		// 	$users['user_role'] = $this->session->userdata('user_role');
+
+		// 	if($users['user_role']=="Student")
+		// 	{
+		// 		redirect('external/homepage');
+		// 	}
+		// 	// check user role is  EA
+		// 	else if ($users['user_role']=="Education Agent")
+		// 	{
+		// 	   redirect('internal/level_2/education_agent/Ea_dashboard');
+		// 	}
+		// 	// check user role is AC
+		// 	else if ($users['user_role']=="Academic Counsellor")
+		// 	{
+		// 	   redirect('internal/level_2/academic_counsellor/Ac_dashboard');
+		// 	}
+		// 	// check user role is E
+		// 	else if ($users['user_role']=="Employer")
+		// 	{
+		// 	   redirect('internal/level_2/employer/Employer_dashboard');
+		// 	}
+		// 	// check user role is  EP
+		// 	else if ($users['user_role']=="Education Partner")
+		// 	{
+		// 	   redirect('internal/level_2/educational_partner/Ep_dashboard');
+		// 	}
+		// }
+    }
+
+    public function index()
+    {
+        $data['title'] = 'Admin | Dashboard';
+        $data['include_js'] = 'admin_dashboard';
+
+        $this->load->view('internal_templates/header');
+        $this->load->view('internal_templates/sidenav');
+        $this->load->view('internal_templates/topbar');
+        $this->load->view('template_view');
+        $this->load->view('internal_templates/footer');
+    }
+
+
+}
---
+++ application/views/template_view.php
@@ -0,0 +1,53 @@
+
+<style>
+th{
+    color:black;
+}
+td{
+    color: rgba(0,0,0,0.7);
+}
+</style>
+<!-- Set base url to javascript variable-->
+<script type="text/javascript">
+    var base_url = "<?php echo base_url();?>";
+</script>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                    <h1 class="h3 mb-0 text-gray-800">Template</h1>
+                </div>
+
+                <!-- Breadcrumn -->
+                <div class="row" >
+                    <div class="breadcrumb-wrapper col-xl-9">
+                        <ol class="breadcrumb" style = "background-color:rgba(0, 0, 0, 0);">
+                            <li class="breadcrumb-item">
+                                <a href="<?php echo base_url('internal/admin_panel/admin_dashboard');?>"><i class="fas fa-tachometer-alt"></i> Home</a>
+                            </li>
+                            <li class="breadcrumb-item active">Template</li>
+                        </ol>
+                    </div>
+                </div>
+                <!-- Content Row (Start here)-->
+                
+                <!-- /. Content Row -->
+
+                </div>
+                <!-- /.container-fluid -->
+
+                </div>
+                <!-- End of Main Content -->
