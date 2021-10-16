9647d2be112ec9d711561519fa221885a3f31fe8
YeePengTheong <yeepengtheong@gmail.com>
2021-10-17 00:01:04 +0800

    rename the login.php to Login.php and create staff_dashboard.php

--- application/controllers/users/login.php
+++ application/controllers/users/login.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class login extends CI_Controller
+class Login extends CI_Controller
 {
     public function __construct()
     {
