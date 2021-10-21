eccf4d1cce8e4cc43229e2fb807d6a4442038d88
LIEWELING <elingliew2019@gmail.com>
2021-10-21 17:39:06 +0800

    Update Employee Dashboard
    1. Delete unnecessary file
    2. Update top navigation bar (color, logo, title)
    3. Update the content of the page (card - total sales, total items, items running low on stock, bar chart)

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -2,8 +2,8 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
-
-	/**
+	
+	/*
 	 * Index Page for this controller.
 	 *
 	 * Maps to the following URL
@@ -11,16 +11,29 @@ class Welcome extends CI_Controller {
 	 *	- or -
 	 * 		http://example.com/index.php/welcome/index
 	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
+	 * Since this controller is set as the default controller in config/routes.php, it's displayed at http://example.com/
 	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 * So any other public methods not prefixed with an underscore will map to /index.php/welcome/<method_name> @see https://codeigniter.com/user_guide/general/urls.html
 	 */
+	
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		// $this->load->view('welcome_message');
 
+		$data['title'] = 'Admin | Dashboard';
+        
+		// $data['include_js'] = 'admin_dashboard';
+
+		// Value for bootstrap css and js plugin that is NOT from SB Admin
+		// $data['bootstrap_css'] = "";
+		// $data['bootstrap_js'] = "";
+
+        $this->load->view('external_templates/header');
+        $this->load->view('external_templates/topnav');
+        
+		// $this->load->view('template_view');
+		
+		$this->load->view('users/employee_dashboard_view');
+        $this->load->view('external_templates/footer');
 	}
 }
