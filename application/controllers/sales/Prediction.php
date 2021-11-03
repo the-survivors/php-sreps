c117ad0b52cfb85cd175355aa5904fdf9dffff80
YeePengTheong <yeepengtheong@gmail.com>
2021-11-04 03:04:57 +0800

    remove var_dump

--- application/controllers/sales/Prediction.php
+++ application/controllers/sales/Prediction.php
@@ -21,7 +21,7 @@ class Prediction extends CI_Controller
         $data['most_sold_item'] = MAX($this->sales_prediction_model->select_most_sold_item($date));
         $data['most_sold_item_subcategory'] = MAX($this->sales_prediction_model->select_most_sold_item_subcategory($date));
 
-        var_dump($data['most_sold_item']);
+        //var_dump($data['most_sold_item']);
        // var_dump($data['most_sold_item_subcategory']);
         $this->load->view('internal_templates/header', $data);
         $this->load->view('sales/prediction_mainpage_view');
