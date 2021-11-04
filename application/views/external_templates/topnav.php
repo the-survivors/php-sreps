78663ea0c17c549ed4d82779336dd6d852ccbebe
Yeo Wei Cheng <77140733+Yeo3170@users.noreply.github.com>
2021-11-04 23:12:13 +0800

    Added dropdown for bell notification button (Hard coded)
    - Back end function still work in progress

--- application/views/external_templates/topnav.php
+++ application/views/external_templates/topnav.php
@@ -81,6 +81,48 @@
                 <!-- Counter - Alerts -->
                 <span class="badge badge-secondary badge-counter text-dark" style="background-color: #FFF1F3;">3+</span>
             </a>
+
+            <!-- Dropdown - Alerts -->
+            <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="alertsDropdown">
+                <h6 class="dropdown-header">
+                    Alerts Center
+                </h6>
+                <a class="dropdown-item d-flex align-items-center" href="#">
+                    <div class="mr-3">
+                        <div class="icon-circle bg-primary">
+                            <i class="fas fa-file-alt text-white"></i>
+                        </div>
+                    </div>
+                    <div class ="text-dark">
+                        <div class="text-primary">Item Name 1</div>
+                        <span class="font-weight-bold">A new monthly report is ready to download!</span>
+                    </div>
+                </a>
+                <a class="dropdown-item d-flex align-items-center" href="#">
+                    <div class="mr-3">
+                        <div class="icon-circle bg-success">
+                            <i class="fas fa-donate text-white"></i>
+                        </div>
+                    </div>
+                    <div class="text-dark">
+                        <div class="text-primary">Item Name 2</div>
+                        $290.29 has been deposited into your account!
+                    </div>
+                </a>
+                <a class="dropdown-item d-flex align-items-center" href="#">
+                    <div class="mr-3">
+                        <div class="icon-circle bg-warning">
+                            <i class="fas fa-exclamation-triangle text-white"></i>
+                        </div>
+                    </div>
+                    <div class="text-dark">
+                        <div class="text-primary">Item Name 3</div>
+                        Spending Alert: We've noticed unusually high spending for your account.
+                    </div>
+                </a>
+                <a class="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>
+            </div>
+        </li>
         </li>
 
         <li class="nav-item pl-1">
